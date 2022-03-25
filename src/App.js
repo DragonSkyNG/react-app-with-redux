69081b8564ff087734b3b2b2481b92f813b220a0
DragonSky_NG <dragonskyng@gmail.com>
2022-03-25 17:55:44 +0200

    Add auth state slice and split redux code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import Counter from './components/Counter';
+import React from "react";
 
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
+
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const authElement = !isAuthenticated ? <Auth /> : <UserProfile />;
   return (
-    <Counter />
+    <React.Fragment>
+      <Header />
+      {authElement}
+      <Counter />
+    </React.Fragment>
   );
 }
 
